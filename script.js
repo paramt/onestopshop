60f3bad981bbd4f0fe4116455412ab65e6600c96
Param Thakkar <pht0104@gmail.com>
2020-11-07 14:54:17 -0500

    Create tabs

--- script.js
+++ script.js
@@ -8,7 +8,27 @@ function moveup() {
 
 function start() {
   moveup();
-  document.getElementById("login").style.display = "grid";
+  document.getElementById("box-container").style.display = "block";
+}
+
+function handleTabClick(event, tabName) {
+  let i, tabcontent, tablinks;
+
+  // This is to clear the previous clicked content.
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Set the tab to be "active".
+  tablinks = document.getElementsByClassName("tab-btn");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Display the clicked tab and set it to active.
+  document.getElementById(tabName).style.display = "grid";
+  event.currentTarget.className += " active";
 }
 
 function select(id) {
