160abe4dd6668eadca4d5ecc5f7ca743e8ea416b
Param Thakkar <pht0104@gmail.com>
2020-11-08 01:31:32 -0500

    Add icons + dropdown menu

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function select(id) {
   itemExistsInList = false;
 
   for (var i = 0; i < selectedItems.length; i++) {
-    if (selectedItems[i] == id) {
+    if (selectedItems[i] == id + ".1") {
       selectedItems.splice(i, i + 1);
       itemExistsInList = true;
     }
@@ -43,8 +43,45 @@ function select(id) {
 
   if (itemExistsInList) {
     document.getElementById("card-" + id).classList.remove("selected");
+    document.getElementById("arrow-" + id).classList.remove("selected");
   } else {
     document.getElementById("card-" + id).classList.add("selected");
-    selectedItems.push(id);
+    document.getElementById("arrow-" + id).classList.add("selected");
+    selectedItems.push(id + ".1");
   }
+}
+
+function dropdown(id) {
+  const menus = document.getElementsByClassName("dropdown-content");
+
+  for (var i = 0; i < menus.length; i++) {
+    if (menus[i].id != "dropdown-" + id) {
+      menus[i].style.display = "none";
+    }
+  }
+
+  if (document.getElementById("dropdown-" + id).style.display == "block") {
+    document.getElementById("dropdown-" + id).style.display = "none";
+  } else {
+    document.getElementById("dropdown-" + id).style.display = "block";
+  }
+}
+
+function selectType(id, typeID) {
+  const buttons = document.getElementsByClassName("select-" + typeID);
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("selected");
+
+    for (var j = 0; j < selectedItems.length; j++) {
+      if (selectedItems[j] == buttons[i].id.split("select-")[1]) {
+        selectedItems.splice(j, j + 1);
+      }
+    }
+  }
+
+  document.getElementById("select-" + id).classList.add("selected");
+  document.getElementById("card-" + typeID).classList.add("selected");
+  document.getElementById("arrow-" + typeID).classList.add("selected");
+  selectedItems.push(id);
 }
